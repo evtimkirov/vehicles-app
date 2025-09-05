13d4d5d64dee230d52268a79902a2a5e4ecb8eeb
Evtim Kirov <evtimkirov@gmail.com>
2025-09-05 14:53:20 +0300

    Vehicle and Trailer entity changes

--- src/Entity/Trailer.php
+++ src/Entity/Trailer.php
@@ -3,15 +3,16 @@
 namespace App\Entity;
 
 use App\Repository\TrailerRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TrailerRepository::class)]
 class Trailer extends Vehicle
 {
-    #[ORM\Column]
+    #[ORM\Column(type: Types::INTEGER)]
     private ?int $load_capacity = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: Types::INTEGER)]
     private ?int $axles = null;
 
     public function getLoadCapacity(): ?int
