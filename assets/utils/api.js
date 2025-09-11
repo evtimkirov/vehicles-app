18d118afed0e3044a582847533bb2feb31e2f15e
Evtim Kirov <evtimkirov@gmail.com>
2025-09-11 23:05:10 +0300

    Redirect when JWT expire

--- assets/utils/api.js
+++ assets/utils/api.js
@@ -1,23 +1,37 @@
 import axios from "axios";
+import AuthStore from "../stores/AuthStore";
 
 const api = axios.create({
     baseURL: "/api/v1/", // Include this before every API endpoint
 });
 
-// Add the interceptor
+/**
+ * Add token to every API endpoint
+ */
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("auth_token");
-
-    if (
-        token &&
-        !["login", "registration", "forgot-password"].some((endpoint) =>
-            config.url.includes(endpoint)
-        )
-    ) {
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
 });
+
+/**
+ * Redirect to the login page if JWT token expired
+ */
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            // Clear the store and the localStorage
+            AuthStore.logout();
+
+            // Redirect to login page
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
